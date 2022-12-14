87e0381ae9f2082a0d8705ae8a2081e495e2f69e
habibashi <habibashi187@gmail.com>
2022-12-14 20:10:22 +0200

    filter to select the year and set the items

--- expense-tracker-app/src/components/enpense-items/ExpenseItem.jsx
+++ expense-tracker-app/src/components/enpense-items/ExpenseItem.jsx
@@ -1,26 +1,14 @@
-import { useState } from "react";
-
 import "./ExpenseItem.css";
 import ExpenseDate from "../expense-date/ExpenseDate";
 import Card from "../card/Card";
 
 const ExpenseItem = (props) => {
-  // working with useState to update title
-  const [title, setTitle] = useState(props.title);
-  console.log('helloo');
-
-  // onClick event
-  const clickHandler = () => {
-    setTitle('Updated!');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
